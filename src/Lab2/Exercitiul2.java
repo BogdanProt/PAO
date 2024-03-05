b165da8813b4ab4a3a2d6677534f2073d28fcb3f
Bogdan Protopopescu <bodipp@Bogdans-MacBook-Air.local>
2024-03-05 17:47:03 +0200

    Finished ex2

--- src/Lab2/Exercitiul2.java
+++ src/Lab2/Exercitiul2.java
@@ -1,7 +1,17 @@
 package Lab2;
+import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Exercitiul2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 3 numbers: ");
+        int[] a = new int[3];
+        for (int i=0; i<3; i++){
+            a[i] = scanner.nextInt();
+        }
+        Arrays.sort(a);
+        System.out.println(a[0] + " < " + a[1] + " < " + a[2]);
     }
 }
