0706da21e5ae0e37a0c85db0d036e8a1d9bc4fa5
Bogdan Protopopescu <bodipp@Bogdans-MacBook-Air.local>
2024-03-05 17:51:17 +0200

    Start ex3 need to finish

--- src/Lab2/Exercitiul3.java
+++ src/Lab2/Exercitiul3.java
@@ -1,2 +1,15 @@
-package Lab2;public class Exercitiul3 {
+package Lab2;
+import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Exercitiul3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+    }
+
+    private static void nthFibonacci() {
+        // To do
+    }
 }
