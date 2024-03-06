646e4e051599a7c68b52bbe26f13a8570067f071
Bogdan Protopopescu <bodipp@Bogdans-MacBook-Air.local>
2024-03-06 11:25:38 +0200

    Finished lab exercises

--- src/Lab2/Exercitiul3.java
+++ src/Lab2/Exercitiul3.java
@@ -4,12 +4,41 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Exercitiul3 {
+
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+
+        nthFibonacci();
+
     }
 
     private static void nthFibonacci() {
-        // To do
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Al catelea termen Fibonacii vrei sa aflii? Introdu un numar:");
+        int n = scanner.nextInt();
+        int last, lastLast, current;
+        int nr = 2;
+        if (n == 1)
+        {
+            System.out.println("Primul termen Fibonacci este: " + 0);
+        }
+        else if (n == 2)
+        {
+            System.out.println("Al doilea termen Fibonacci este: " + 1);
+        }
+        else{
+            current = 0;
+            lastLast = 0;
+            last = 1;
+            while (nr < n)
+            {
+                nr = nr + 1;
+                current = last + lastLast;
+                lastLast = last;
+                last = current;
+            }
+            System.out.println("Al " + n + "-lea termen Fibonacci este: " + current);
+        }
+
     }
 }
